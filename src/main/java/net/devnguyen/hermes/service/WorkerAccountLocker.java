851d0944e0a18b8bd6ddf7e2abdc748f26e41550
Nguyen Manh Hung <hungnm@remitano.com>
2024-11-02 18:06:04 +0700

    Worker Account lock update keep partition

--- src/main/java/net/devnguyen/hermes/service/WorkerAccountLocker.java
+++ src/main/java/net/devnguyen/hermes/service/WorkerAccountLocker.java
@@ -1,6 +1,8 @@
 package net.devnguyen.hermes.service;
 
 import lombok.extern.slf4j.Slf4j;
+import net.devnguyen.hermes.HermesApplication;
+import net.devnguyen.hermes.dto.ResponseDTO;
 import org.redisson.api.RScript;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
@@ -19,21 +21,22 @@ public class WorkerAccountLocker {
         this.baseScript = luaScriptLoader.worker();
     }
 
-    public String accountRedisKey(String accountId) {
+    private String accountRedisKey(String accountId) {
         return String.format("hermes:account:%s", accountId);
     }
 
-    public String workerRedisKey(String workerId) {
+    private String workerRedisKey(String workerId) {
         return String.format("hermes:list_account:%s", workerId);
     }
 
-    public String luaScript(String script) {
+    private String luaScript(String script) {
         return String.join("\n", baseScript, script);
     }
 
 
-    public boolean pickAccount(String workerId, String accountId) {
+    public boolean pickAccount(String accountId) {
         int ttl = 30;
+        var workerId = HermesApplication.ID;
         String script = luaScript("""
                 local worker_id = ARGV[1];
                 local account_id = ARGV[2];
@@ -60,7 +63,7 @@ public class WorkerAccountLocker {
         return ok;
     }
 
-    public void unpickAccount(String workerId, String accountId) {
+    public void unpickAccount(String accountId) {
         String script = luaScript("""
                 local worker_id = ARGV[1];
                 local account_id = ARGV[2];
@@ -68,7 +71,7 @@ public class WorkerAccountLocker {
                 
                 return Worker.unpick_account(worker_id, account_id);
                 """);
-
+        var workerId = HermesApplication.ID;
         List<Object> lockKeys = new ArrayList<>(List.of(workerRedisKey(workerId), accountRedisKey(accountId)));
 
         var resultScript = redissonClient.getScript()
@@ -86,23 +89,103 @@ public class WorkerAccountLocker {
         }
     }
 
-    public void shutdown(String workerId) {
+    public void shutdown() {
         String script = luaScript("""
                 local worker_id = ARGV[1];
                 
                 return Worker.shutdown(worker_id);
                 """);
 
-        List<Object> lockKeys = new ArrayList<>(List.of(workerRedisKey(workerId)));
+        List<Object> lockKeys = new ArrayList<>(List.of(workerRedisKey(HermesApplication.ID)));
 
         var resultScript = redissonClient.getScript()
                 .eval(RScript.Mode.READ_WRITE,
                         script,
                         RScript.ReturnType.MULTI,
                         lockKeys,
-                        workerId);
+                        HermesApplication.ID);
 
         log.info("shutdown: lua script result: {}", resultScript);
     }
+
+    public ResponseDTO<Integer> getPartitionRouterForAccount(String accountId, int defaultPartition, boolean isPickAccount) {
+        String script = luaScript("""
+                local account_id = ARGV[1];
+                local default_partition_id = ARGV[2];
+                local is_pick_account = ARGV[3];
+                return Worker.kafka_router_partition(account_id, default_partition_id, is_pick_account);
+                """);
+
+        var resultScript = (List<String>) redissonClient.getScript()
+                .eval(RScript.Mode.READ_WRITE,
+                        script,
+                        RScript.ReturnType.MULTI,
+                        new ArrayList<>(),
+                        HermesApplication.ID, accountId, defaultPartition + "", isPickAccount ? "ok" : "false");
+
+        var ok = resultScript.get(0).equalsIgnoreCase("OK");
+        if (!ok) {
+            return ResponseDTO.fail(String.join(",", resultScript), -1);
+        }
+
+        var partitionId = resultScript.get(1);
+        return ResponseDTO.success(Integer.parseInt(partitionId));
+    }
+
+
+    public void kafkaAssignedPartition(String partitionId) {
+        String script = luaScript("""
+                local worker_id = ARGV[1];
+                local partition = ARGV[2];
+                
+                return Worker.kafka_assigned_partition(worker_id, partition);
+                """);
+
+        List<Object> lockKeys = new ArrayList<>();
+
+        redissonClient.getScript()
+                .eval(RScript.Mode.READ_WRITE,
+                        script,
+                        RScript.ReturnType.MULTI,
+                        lockKeys,
+                        HermesApplication.ID,
+                        partitionId);
+    }
+
+    public void kafkaRevokePartition(String partitionId) {
+        String script = luaScript("""
+                local worker_id = ARGV[1];
+                local partition = ARGV[2];
+                
+                return Worker.kafka_revoke_partition(worker_id, partition);
+                """);
+
+        List<Object> lockKeys = new ArrayList<>();
+
+        redissonClient.getScript()
+                .eval(RScript.Mode.READ_WRITE,
+                        script,
+                        RScript.ReturnType.MULTI,
+                        lockKeys,
+                        HermesApplication.ID, partitionId);
+    }
+
+    public void kafkaDelPartition() {
+        String script = luaScript("""
+                local worker_id = ARGV[1];
+                
+                return Worker.kafka_worker_shutdown(worker_id);
+                """);
+
+        List<Object> lockKeys = new ArrayList<>();
+
+        redissonClient.getScript()
+                .eval(RScript.Mode.READ_WRITE,
+                        script,
+                        RScript.ReturnType.MULTI,
+                        lockKeys,
+                        HermesApplication.ID);
+    }
+
 
 }
--- src/main/resources/lua-scripts/worker.lua
+++ src/main/resources/lua-scripts/worker.lua
@@ -5,6 +5,12 @@ function Worker.pick_account(worker_id, account_id, ttl)
     local current_worker_id = redis.call("GET", account_key)
 
     if Worker.is_empty(current_worker_id) or current_worker_id == worker_id then
+        --local current_ttl = redis.call("TTL", account_key)
+        --
+        --if current_ttl < 0 or current_ttl < ttl or Worker.is_empty(current_ttl) then
+        --    current_ttl = ttl
+        --end
+
         redis.call("SET", account_key, worker_id, "EX", ttl)
         Worker.add_account(worker_id, account_id)
         return { "ok", "" }
@@ -22,18 +28,21 @@ function Worker.unpick_account(worker_id, account_id)
     if current_worker_id == worker_id then
         redis.call("DEL", account_key)
         return { "ok", "" }
+    elseif Worker.is_empty(current_worker_id) then
+        return { "ok", "" }
     else
         return { "fail", "current worker is not you" }
     end
-
 end
 
 function Worker.add_account(worker_id, account_id)
     redis.call("SADD", Worker.list_account_key(worker_id), account_id)
+    --redis.call("EXPIRE", Worker.list_account_key(worker_id), 3600) -- set ttl 1h
 end
 
 function Worker.remove_account(worker_id, account_id)
     redis.call("SREM", Worker.list_account_key(worker_id), account_id)
+    --redis.call("EXPIRE", Worker.list_account_key(worker_id), 3600)
 end
 
 function Worker.shutdown(worker_id)
@@ -56,6 +65,66 @@ end
 
 function Worker.account_key(account_id)
     return string.format("hermes:account:%s", account_id)
+end
+
+-- kafka manager
+
+function Worker.kafka_router_partition(account_id, default_partition_id, is_pick_account)
+    local account_key = Worker.account_key(account_id)
+    local worker_id = redis.call("GET", account_key)
+    if Worker.is_empty(worker_id) then
+        if is_pick_account == "ok" then
+            -- hold
+            Worker.pick_account(worker_id, default_partition_id, 10)
+        end
+
+        return { "ok", default_partition_id, "default default_partition_id" }
+    end
+
+    local partition_worker_key = Worker.kafka_assigned_partition_key(worker_id)
+
+    local partition_ids = redis.call("SMEMBERS", partition_worker_key)
+
+    -- Check if partition_ids is empty
+    if #partition_ids == 0 then
+        return { "error", "No partitions assigned to worker", "-1" }
+    end
+
+    if is_pick_account == "ok" then
+        -- hold
+        Worker.pick_account(worker_id, account_id, 10)
+    end
+
+    -- Return the first partition if there are partitions available
+    return { "ok", partition_ids[1] }
+end
+
+function Worker.kafka_assigned_partition(worker_id, partition)
+    local key = Worker.kafka_assigned_partition_key(worker_id)
+    redis.call("SADD", key, partition)
+    redis.call("SET", Worker.kafka_partition_key(partition), worker_id)
+    return { "ok", "" }
+end
+
+function Worker.kafka_revoke_partition(worker_id, partition)
+    local key = Worker.kafka_assigned_partition_key(worker_id)
+    redis.call("SREM", key, partition)
+    redis.call("DEL", Worker.kafka_partition_key(partition), worker_id)
+    return { "ok", "" }
+end
+
+function Worker.kafka_worker_shutdown(worker_id)
+    local key = Worker.kafka_assigned_partition_key(worker_id)
+    redis.call("DEL", key)
+    return { "ok", "" }
+end
+
+function Worker.kafka_assigned_partition_key(worker_id)
+    return string.format("hermes:kafka:assigned_partition:%s", worker_id)
+end
+
+function Worker.kafka_partition_key(partition)
+    return string.format("hermes:kafka:partition:%s", partition)
 end
 
 function Worker.is_empty(data)
